33a6978f439e7436bc1d545c23fdb84113865f9c
moonelysian <yejimun.dev@gmail.com>
2024-03-21 15:42:46 +0900

    add: 무한스크롤 추가

--- src/List/PokeCardList.tsx
+++ src/List/PokeCardList.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import PokeCard from "./PokeCard";
 import styled from "@emotion/styled";
+import useInfiniteScroll from "react-infinite-scroll-hook";
+import PokeCard from "./PokeCard";
 import {
   fetchPokemons,
   PokemonListResponseType,
@@ -13,12 +14,28 @@ const PokeCardList = () => {
     previous: null,
     results: [],
   });
+
   useEffect(() => {
     (async () => {
       const pokemons = await fetchPokemons();
       setPokemons(pokemons);
     })();
   }, []);
+
+  const [sentryRef] = useInfiniteScroll({
+    loading: false,
+    hasNextPage: pokemons.next !== "",
+    onLoadMore: async () => {
+      const moreData = await fetchPokemons(pokemons.next);
+      setPokemons({
+        ...moreData,
+        results: [...pokemons.results, ...moreData.results],
+      });
+    },
+    disabled: false,
+    rootMargin: "0px 0px 400px 0px",
+  });
+
   return (
     <>
       <List>
@@ -26,6 +43,7 @@ const PokeCardList = () => {
           <PokeCard key={`${pokemon.name}-${idx}`} name={pokemon.name} />
         ))}
       </List>
+      <Loading ref={sentryRef}>Loading...</Loading>
     </>
   );
 };
@@ -38,6 +56,11 @@ const List = styled.ul`
   gap: 20px;
   flex-direction: row;
   flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const Loading = styled.div`
+  display: flex;
   justify-content: center;
 `;
 
